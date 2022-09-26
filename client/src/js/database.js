413681a455c94c893c4594db6eb90fb12a178463
Jordan Heidlage <jordanheidlage@jordans-air.lan>
2022-09-26 15:19:56 -0400

    fixing connection issues

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -1,16 +1,40 @@
 import { openDB } from 'idb';
 
 const initdb = async () =>
+// We are creating a new database named 'jate' which will be using version 1 of the database.
   openDB('jate', 1, {
+        // Add our database schema if it has not already been initialized.
     upgrade(db) {
       if (db.objectStoreNames.contains('jate')) {
         console.log('jate database already exists');
         return;
       }
+            // Create a new object store for the data and give it an key name of 'id' which needs to increment automatically.
       db.createObjectStore('jate', { keyPath: 'id', autoIncrement: true });
       console.log('jate database created');
     },
   });
+
+// DONE: Add logic for a method that gets all the content from the database
+export const getDb = async () => {
+  console.log('Retrieving data from the DB', result);
+    // Create a connection to the database database and version we want to use.
+    const jateDb  = await openDB('jate', 1);
+  
+    // Create a new transaction and specify the database and data privileges.
+    const tx = jateDb.transaction('jate', 'readonly');
+  
+    // Open up the desired object store.
+    const store = tx.objectStore('jate');
+  
+    // Use the .getAll() method to grab all content in DB 
+    const request = store.getAll();
+  
+    // Get confirmation of the request.
+    const result = await request;
+    console.log('result.value', result);
+    return result;
+  };
 
 // DONE: Add logic to a method that accepts some content and adds it to the database
 export const putDb = async (content) => {
@@ -19,13 +43,13 @@ console.log('data saved to database', result);
   const jateDb  = await openDB('jate', 1);
 
   // Create a new transaction and specify the database and data privileges.
-  const tx = jateDb.transaction('jate', 'readonly');
+  const tx = jateDb.transaction('jate', 'readwrite');
 
   // Open up the desired object store.
   const store = tx.objectStore('jate');
 
   // Use the .put() method 
-  const request = store.put({ id: 1, value: content });
+  const request = store.put({ id: 1, jate: content });
 
   // Get confirmation of the request.
   const result = await request;
@@ -33,24 +57,5 @@ console.log('data saved to database', result);
   return result;
 };
 
-// DONE: Add logic for a method that gets all the content from the database
-export const getDb = async () => {
-console.log('Retrieving data from the DB', result);
-  // Create a connection to the database database and version we want to use.
-  const jateDb  = await openDB('jate', 1);
 
-  // Create a new transaction and specify the database and data privileges.
-  const tx = jateDb.transaction('jate', 'readonly');
-
-  // Open up the desired object store.
-  const store = tx.objectStore('jate');
-
-  // Use the .getAll() method to grab all content in DB 
-  const request = store.getAll();
-
-  // Get confirmation of the request.
-  const result = await request;
-  console.log('result.value', result);
-  return result;
-};
 initdb();
--- client/webpack.config.js
+++ client/webpack.config.js
@@ -39,8 +39,8 @@ module.exports = () => {
         description: 'A text editor using IndexedDB to implement offline capability',
         background_color: '#225ca3',
         theme_color: '#225ca3',
-        start_url: '/',
-        publicPath: '/',
+        start_url: './',
+        publicPath: './',
         icons: [
           {
             src: path.resolve('src/images/logo.png'),
